aab8fa052deb90f2d39efc8de948ac772650b722
M45Hasan <mmhasan045@gmail.com>
2022-09-15 13:13:22 +0600

    upto__recentwork

---
+++ js/tailwind.config.js
@@ -0,0 +1,34 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: "#da373d",
+        white: "rgba(238, 237, 222, 1)",
+        red: "#F40404",
+        yellow: "rgb(250 204 21)",
+        bgclr: "rgba(255, 255, 255, .8)",
+        pri: "rgba(106, 77, 244, 1)",
+        sec: "rgba(255, 196, 74, 1)",
+        thr: "rgba(74, 146, 255, 1)",
+        bg1: "#F8F6FE",
+        bg2: "#F3F3F3",
+        tx1:"#737373",
+      },
+      maxWidth: {
+        container: "1320px",
+      },
+      fontFamily: {
+        pop: "Poppins",
+        int:"Inter",
+      },
+      backgroundImage: {
+        btmban: "url('btmban.png')",
+        topBan: "url('topBan.png')",
+        crvSky: "url('crvSky.png')",
+        crvBlue: "url('crvBlue.png')",
+        logo: "url('logo.png')",
+
+      },
+    },
+  },
+};
